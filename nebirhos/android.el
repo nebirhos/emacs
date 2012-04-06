f6d7ac37a47ba3903c865143b5f5145c480da8a0
nebirhos <nebirhos@aol.com>
2012-04-07 01:30:21 +0200

    Android SDK path sourced from env

--- nebirhos/android.el
+++ nebirhos/android.el
@@ -1,4 +1,6 @@
 ;; Android SDK mode
 (vendor 'android-mode)
 
-(setq android-mode-sdk-dir '/usr/local/android-sdk-linux)
+(setq android-mode-sdk-dir '/opt/android-sdk-linux)
+(if (getenv "ANDROID_SDK")
+    (setq android-mode-sdk-dir (getenv "ANDROID_SDK")) )
