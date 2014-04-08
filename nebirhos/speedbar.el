33e95d866cbd95476044ef5cafe7c5268a9786df
nebirhos <me@nebirhos.com>
2014-04-08 11:10:05 +0200

    No auto-refresh for SpeedBar

--- nebirhos/speedbar.el
+++ nebirhos/speedbar.el
@@ -4,7 +4,7 @@
 (setq sr-speedbar-right-side nil) ; speedbar on left pane
 (setq speedbar-show-unknown-files t)
 (setq speedbar-use-images nil)
-(setq sr-speedbar-auto-refresh t)
+(setq sr-speedbar-auto-refresh nil)
 
 ;; key bindings
 (global-set-key [f11] 'sr-speedbar-toggle)
